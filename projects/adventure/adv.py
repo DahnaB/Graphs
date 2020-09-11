835f0f4212a9c6d19fc756f56b968cef7b81c854
Dahna <dbuenrostro09@gmail.com>
2020-09-11 11:09:13 -0400

    sprint-challenge-mvp

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -2,8 +2,22 @@ from room import Room
 from player import Player
 from world import World
 
+
 import random
 from ast import literal_eval
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
 
 # Load world
 world = World()
@@ -25,11 +39,49 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+visited = set()
+
+backtrack = {"n": "s", "s": "n", "e": "w", "w": "e"}
+
+stack = Stack()
+
+direction_dict = {}
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
 
 
+def traverse_maze():
+    while len(visited) < len(room_graph):
+        current_room_id = player.current_room.id 
+        if current_room_id not in visited:
+            visited.add(current_room_id)
+            exits = player.current_room.get_exits()
+            direction_dict[current_room_id] = exits
+
+        num_exits = len(direction_dict[current_room_id])
+        
+        while num_exits > -1:
+            # checking that we are not at a dead end
+            if num_exits is not 0:
+                direction = direction_dict[current_room_id].pop()
+                if player.current_room.get_room_in_direction(direction).id not in visited:
+                    stack.push(direction)
+                    player.travel(direction)
+                    traversal_path.append(direction)
+                    break
+
+            num_exits = len(direction_dict[current_room_id])
+            # However, if we do have a deadend, have player retrace steps
+            if num_exits is 0:
+                prev_move = stack.pop()
+                prev_direction = backtrack[prev_move]
+                player.travel(prev_direction)
+                traversal_path.append(prev_direction)
+                break
+
+traverse_maze()
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -51,12 +103,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,28 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    graph = Graph()
+
+    # Add Vertices
+    for vertex_1, vertex_2 in ancestors:
+        graph.add_vertex(vertex_1)
+        graph.add_vertex(vertex_2)
+
+    # Add Edges
+    for vertex_1, vertex_2 in ancestors:
+        graph.add_edge(vertex_1, vertex_2)
+
+    target_vertex = None
+    longest_path = 1
+    for vertex in graph.vertices:
+        # vertex is start, starting_node is destination
+        path = graph.dfs(vertex, starting_node)
+        if path:
+            if len(path) > longest_path:
+                longest_path = len(path)
+                target_vertex = vertex
+        elif not path and longest_path == 1:
+            target_vertex = -1
+
+    return target_vertex
+
